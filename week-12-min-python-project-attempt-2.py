784ee93be018d17467832880d4c2156e6b7bf7db
tom-puntillo <tom.puntillo@gmail.com>
2023-06-04 06:05:56 +0000

    modified week-12-project

--- week-12-min-python-project-attempt-2.py
+++ week-12-min-python-project-attempt-2.py
@@ -1,13 +1,7 @@
 import random
 
-ec2list = []
-
 instances_needed = int(input('How many EC2 Instances do you need? '))
 department_name = input('What department are you in? ')
 
 for n in range(instances_needed):
-    r = random.randint(1,instances_needed)
-    if r not in ec2list:
-        ec2list.append(r)
-        
-    print(department_name,'-', r)
+    print(department_name, random.randrange(instances_needed), random.randrange(instances_needed), sep='_')
