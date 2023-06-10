64f7693fd153c38762c1032f86b851a7d6a87849
tom-puntillo <tom.puntillo@gmail.com>
2023-06-10 20:59:32 +0000

    renamed files

---
+++ week-13-project-practice-staging.py
@@ -0,0 +1,16 @@
+import os
+
+os.chdir('/home/ec2-user/environment/blue-python-repo')
+file_list = []
+
+for name in os.listdir():
+    size = os.stat(name).st_size
+    
+    file_dict = {
+        'Name' : name,
+        'Size' : size
+    }
+    
+    file_list.append(file_dict)
+    
+print(file_list)
