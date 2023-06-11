e7c97b881907f2216b82ab350f8afb40effd8527
tom-puntillo <tom.puntillo@gmail.com>
2023-06-11 08:05:47 +0000

    updated practice file

---
+++ week-13-advance-practice-2.py
@@ -0,0 +1,20 @@
+import os
+
+def print_directory():
+
+    file_list = []  # Create an empty list to store file information
+    
+    
+    for name in os.listdir():  # Iterate over the files in the current directory
+        size = os.stat(name).st_size  # Get the size of the current file
+        
+        file_dict = {  # Create a dictionary to store file name and size
+            'Name' : name,
+            'Size' : size
+        }
+        
+        file_list.append(file_dict)  # Add the dictionary to the file_list
+        
+    print(file_list)  # Print the list containing file information
+
+print_directory()
